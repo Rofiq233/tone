fc248da09ff7e61ca9e7b7b4c0b72084b8b4774b
Md. Rofiqul islam <rofiqulislamrobiul23@gmail.com>
2026-07-10 21:17:13 +0600

    faq's added

--- assets/custom.js
+++ assets/custom.js
@@ -1,1 +1,62 @@
-  
+  if (!customElements.get("custom-accordion")) {
+  customElements.define(
+    "custom-accordion",
+    class CustomAccordion extends HTMLElement {
+      constructor() {
+        super();
+ 
+        this.isFaq = this.querySelector(".faq-item");
+        this.isBlog = this.querySelector(".blog-category-item");
+
+        if (this.isFaq) {
+          this.item = this.querySelector(".faq-item");
+          this.panel = this.querySelector(".faq-item-content");
+          this.trigger = this.item; 
+        }
+
+        if (this.isBlog) {
+          this.item = this.querySelector(".blog-category-item");
+          this.panel = this.querySelector(".blog-subcategory-list");
+          this.trigger = this.querySelector(".blog-category-toggle");
+        }
+      }
+
+      connectedCallback() {
+        if (!this.item || !this.panel || !this.trigger) return;
+
+        if (this.item.classList.contains("is-open")) {
+          this.panel.style.height = this.panel.scrollHeight + "px";
+        }
+
+        this.trigger.addEventListener("click", (e) => {
+          e.preventDefault();
+          this.toggle();
+        });
+      }
+
+      toggle() {
+        this.item.classList.contains("is-open")
+          ? this.close()
+          : this.open();
+      }
+
+      open() {
+        this.item.classList.add("is-open");
+        this.panel.style.height = "0px";
+
+        requestAnimationFrame(() => {
+          this.panel.style.height = this.panel.scrollHeight + "px";
+        });
+      }
+
+      close() {
+        this.panel.style.height = this.panel.scrollHeight + "px";
+        this.item.classList.remove("is-open");
+
+        requestAnimationFrame(() => {
+          this.panel.style.height = "0px";
+        });
+      }
+    }
+  );
+}
